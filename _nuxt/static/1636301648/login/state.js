07db475ae07176ec4d929305063d7ef75310ea39
Dang Pham Nhat Thanh <dpnthanh@gmail.com>
2021-11-07 23:21:59 +0700

    update project

---
+++ _nuxt/static/1636301648/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636301648",layout:"default",error:a,state:{auth:{accessToken:b},app:{mode:"main",result:false,phone:b}},serverRendered:true,routePath:"\u002Flogin",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,""));
